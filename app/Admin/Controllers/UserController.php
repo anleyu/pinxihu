313ee6baa004c318d4f60f6ae3d84e6bac02b0e3
Administrator <501592104@qq.com>
2017-01-09 11:09:02 +0800

    ok

---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\User;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class UserController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('会员列表');
+            $content->description('管理普通用户');
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+
+            $content->header('编辑用户');
+            $content->description('编辑用户信息');
+
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('新增用户');
+            $content->description('添加新用户');
+
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(User::class, function (Grid $grid) {
+
+            $grid->id('编号')->sortable();
+            $grid->name('姓名');
+            $grid->email('邮箱');
+            $grid->created_at('新增时间');
+            $grid->updated_at('更新时间');
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Admin::form(User::class, function (Form $form) {
+
+            $form->display('id', '编号');
+            $form->text('name', '姓名')->rules('required');
+            $form->password('password', '密码')->rules('required');
+            $form->email('email', '邮箱');
+            $form->display('created_at', '新增时间');
+            $form->display('updated_at', '更新时间');
+            $form->saving(function (Form $form) {
+
+                    $form->password = bcrypt($form->password);
+
+            });
+        });
+    }
+}
---
+++ app/Admin/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Route::group([
+    'prefix'        => config('admin.prefix'),
+    'namespace'     => Admin::controllerNamespace(),
+    'middleware'    => ['web', 'admin'],
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index');
+    $router->resource('users', UserController::class);
+
+});
+
+
+
+
